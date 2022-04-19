c5b1bc975824e9a0f7c7463cbadcd6ad4adffd29
kaskiv <kaskiv11gmail.com>
2022-04-19 13:45:25 +0300

    Add project files.

---
+++ Lab4.1C/Lab4.1C.cpp
@@ -0,0 +1,34 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <stdio.h>
+
+using namespace std;
+
+
+int ProcessTXT(char* fname) {
+	FILE* f;
+	if ((f = fopen(fname, "r")) == NULL) {
+		cerr << "Помилка при відкритті файлу.\n";
+		exit(1);
+	};
+	char ch = getc(f);
+	int k = 0;
+	while (ch != EOF)
+	{
+		if (ch == 'B' || ch == 'A' || ch == 'S' || ch == 'I' || ch == 'C') {
+			k++;
+		}
+		ch = getc(f);
+	}
+	fclose(f);
+	return k;
+}
+
+int main()
+{
+	char name[6] = "t.txt";
+	cout << "Count of symbols which included in word BASIC: " << ProcessTXT(name) << endl;
+
+	cin.get();
+	return 0;
+}
---
+++ UnitTestLab4.1C/UnitTestLab4.1C.cpp
@@ -0,0 +1,22 @@
+﻿#include "pch.h"
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include "CppUnitTest.h"
+#include "../Lab4.1C/Lab4.1C.cpp"
+
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTestLab41C
+{
+	TEST_CLASS(UnitTestLab41C)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			int t = ProcessTXT("../t.txt");
+			Assert::AreEqual(t, 8);
+		}
+	};
+}
